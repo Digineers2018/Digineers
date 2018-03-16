5a4ad017da437f95b352ba5b2b5826dbb4f29f39
Sachin <sachinpatel248@gmail.com>
2018-03-16 17:08:27 +0530

    WebApplication

---
+++ WebApplication1/WebApplication1/Controllers/ImageAPIController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+using System.Drawing;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    public class ImageAPIController : ApiController
+    {
+
+        const string SUBSCRIBTION_KEY = "d51fdfb00e354267bd91d120d92b2f70";
+        const string VOICE_OCP_AICP_SUBSCRIPTION_KEY = "Ocp-Apim-Subscription-Key";
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        public Bitmap input_Face_Image;
+        byte[] image_Byte_Data;
+
+
+        //static async void MakeRequest()
+        //{
+        //    var client = new HttpClient();
+        //    var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+        //    // Request headers
+        //    client.DefaultRequestHeaders.Add(VOICE_OCP_AICP_SUBSCRIPTION_KEY, VOICE_SUBSCRIBTION_KEY);
+
+        //    // Request parameters
+        //    queryString["shortAudio"] = "false";
+        //    var uri = "https://westus.api.cognitive.microsoft.com/spid/v1.0/identificationProfiles/{identificationProfileId}/enroll?" + queryString;
+
+        //    HttpResponseMessage response;
+
+        //    // Request body
+        //    byte[] byteData = Encoding.UTF8.GetBytes("{body}");
+
+        //    using (var content = new ByteArrayContent(byteData))
+        //    {
+        //        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        //        response = await client.PostAsync(uri, content);
+        //    }
+
+        //}
+
+        /// <summary>
+        /// Gets the analysis of the specified image file by using the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file.</param>
+        static async void MakeAnalysisRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", SUBSCRIBTION_KEY);
+
+            // Request parameters. A third optional parameter is "details".
+            string requestParameters = "returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                // Execute the REST API call.
+                response = await client.PostAsync(uri, content);
+
+                // Get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                dynamic dynObj = JsonConvert.DeserializeObject(contentString);
+
+            }
+        }
+
+        //    /api/ImageAPI/ImageDemo
+        [HttpGet()]
+        public void ImageDemo()
+        {
+            string image_File_Path = @"C:\Users\Sachin\Desktop\GG.jpg";
+
+            //image_Byte_Data = GetImageAsByteArray(image_File_Path);
+            //input_Face_Image = new Bitmap(image_File_Path);
+            MakeAnalysisRequest(image_File_Path);
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        public void New_Enrollment_Get_Key()
+        {
+            
+        }
+
+
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+
+    }
+}
